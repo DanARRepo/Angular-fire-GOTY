ddd4d7fee475bd254a85794c98e4c0d3786c41d6
Daniel <daniel.amayar001@gmail.com>
2023-08-22 22:10:52 -0500

    Revert "feat: :sparkles: Front end done"
    This reverts commit 48e1579e2e9c241fe2fc1e843cb87450a589829f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ComponentsModule } from './components/components.module';
-
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './pages/homepage/homepage.component';
-import { GotyComponent } from './pages/goty/goty.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomepageComponent,
-    GotyComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ComponentsModule,
-    HttpClientModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
